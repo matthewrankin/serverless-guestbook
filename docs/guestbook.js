d19fd87c67f82722d20cc7e5630fc2e3cb063efa
Matthew Rankin <matthewrankin@users.noreply.github.com>
2019-04-27 18:22:42 -0500

    Correct API URL.

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://aef0b23d.us-south.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `https://aef0b23d.us-south.apiconnect.appdomain.cloud/guestbook/entries`,
+      url: `${apiUrl}/entries`,
       dataType: 'json'
     });
   },
